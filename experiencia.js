fa36c0dd3b99323bb0f9bc8bacaa7c597dc1180a
GodoyLP <126925948+GodoyLP@users.noreply.github.com>
2023-03-04 00:26:32 -0300

    Actualizar experiencia laborales

--- experiencia.js
+++ experiencia.js
@@ -17,21 +17,21 @@ class Expereincia {
 
 /*Todas las expecienca laborales oredenad de la primera a la ultima orden cronologico*/
 let experiencia1 = new Expereincia("02/2016","04/2016", "Puesto Mantenimiento y soporte técnico en Empresa Nelo Diseño S.A.",
-  "Control de equipamiento y reparación.", "Mantenimiento de equipos informático e impresora.", "Conexiones de redes.", "Antivirus.", "Conexiones remotas.");
+  "Control de equipamiento y reparación.", "Mantenimiento de equipos informático e impresora.","Actualizacion de software", "Conexiones de redes.", "Antivirus.", "Conexiones remotas.");
 let experiencia2 = new Expereincia("06/2016","01/2017","Puesto vendedor técnico en Home PC.","Asesoramiento y venta de insumos informáticos.",
-"Creación de inventarios de insumos.","Mantenimiento y reparación de ordenadores personales e impresoras.","Soporte técnico.",);
+"Creación de inventarios de insumos.","Mantenimiento y reparación de ordenadores personales e impresoras.","Soporte técnico.","Ateción al cliente","Creación de ticket.",);
 let experiencia3 = new Expereincia( "02/2017","08/2018",  "Puesto administración aula virtual en forma particular (free lance)",
-  "Crear premisos a los estudiantes.",  "Postear material de estudio.",  "Creación de tareas hogareñas.",  "Preparación de material administrativo del docente.",);
+  "Crear permisos a los estudiantes.",  "Postear material de estudio.",  "Creación de tareas hogareñas.",  "Preparación de material administrativo del docente.","Comunicación con tutores.","Envio y recepcion de e-mail.",);
 let experiencia4 = new Expereincia("02/2018","12/2019","Puesto Clases particulares y soporte técnico en forma particular.",
-"Clases didácticas de acuerdo a las necesidades del alumno.","Creación de apuntes de estudio.","Mantenimiento de equipos e impresoras.","Conexiones de redes.","Instalaciones de programas, antivirus y configuración.",);
+"Clases didácticas de acuerdo a las necesidades del alumno.","Creación de apuntes de estudio.","Mantenimiento de equipos e impresoras.","Conexiones de redes.","Instalaciones de programas, antivirus y configuración.","Reparaciones forma virtual.",);
 let experiencia5 = new Expereincia("10/2018", "08/2019", "Puesto C.A.U. Instalaciones en Empresa YPF S.A.", "Entrega de activos informático.", "Instalación de imagen corporativa.", "Instalación de programas homologados.",
 "Reposición de activos informáticos.", "Control de los activos informáticos en actividad.", "Apertura y cierre de peticiones en HP Manager.",);
-let experiencia6 = new Expereincia("08/2019","02/2020","Puesto Soporte Técnico en Empresa Banco Credicoop.","Remplazo de activos informático. Mudanza de   puesto de trabajo.",
-"Migración de datos.","Instalación de imagen corporativa.","Instalación de programas homologados.",);
+let experiencia6 = new Expereincia("08/2019","02/2020","Puesto Soporte Técnico en Empresa Banco Credicoop.","Remplazo de activos informático."," Mudanza de puesto de trabajo.",
+"Migración de datos.","Creacion de imagens corporativa.","Instalación de imagen corporativa.","Instalación de programas homologados.",);
 let experiencia7 = new Expereincia("02/2020","Actualidad","Puesto Soporte Técnico Administrativo En forma particular.",
 "Creación plataforma virtual y administración.","Creación de actividades y trabajos prácticos.","Administración de correos y respuesta.","Llenado de planillas y documentación.","Mantención de ordenadores personales.","Conexión remota.",);
-let experiencia8 = new Expereincia("01/2023","Actualidad","Reparacion Celular & Pcs (forma particular).","Dectecion de fallas.","Cambio de componentes defectuoso.","Instalcion de software.",
-);
+let experiencia8 = new Expereincia("01/2023","Actualidad","Reparacion Celular & Pcs (forma particular).","Dectecion de fallas.","Cambio de componentes defectuoso.","Instalcion de software.","Actualizaciones","Cambio de pantalla.","Remplazo de componente IC",);
+
 
 /*funcion muestra una expereincia laboral*/
 function experienciaLAboral(experiencia) {
